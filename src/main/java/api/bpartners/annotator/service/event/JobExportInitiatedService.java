c07a5813151c441cf436852aff5512899a2af238
Mahefaa <hei.mahefa@gmail.com>
2024-06-27 16:22:17 +0300

    chore: split batches using ListGrouper

--- src/main/java/api/bpartners/annotator/service/event/JobExportInitiatedService.java
+++ src/main/java/api/bpartners/annotator/service/event/JobExportInitiatedService.java
@@ -2,6 +2,7 @@ package api.bpartners.annotator.service.event;
 
 import static java.util.UUID.randomUUID;
 
+import api.bpartners.annotator.datastructure.ListGrouper;
 import api.bpartners.annotator.endpoint.event.EventProducer;
 import api.bpartners.annotator.endpoint.event.model.ExportTaskCreated;
 import api.bpartners.annotator.endpoint.event.model.JobExportInitiated;
@@ -10,23 +11,25 @@ import api.bpartners.annotator.repository.model.AnnotationBatch;
 import api.bpartners.annotator.repository.model.ExportTask;
 import api.bpartners.annotator.service.AnnotationBatchService;
 import api.bpartners.annotator.service.ExportTaskService;
-import com.google.common.collect.Lists;
 import jakarta.mail.internet.InternetAddress;
 import java.util.List;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class JobExportInitiatedService implements Consumer<JobExportInitiated> {
   // TODO: put as env vars
   private static final int BATCH_PARTITION_SIZE = 1000;
   private static final int MAX_HANDLED_BATCH_SIZE = 1200;
   private final AnnotationBatchService annotationBatchService;
   private final ExportTaskService exportTaskService;
-  private final EventProducer eventProducer;
+  private final EventProducer<ExportTaskCreated> eventProducer;
+  private final ListGrouper<AnnotationBatch> listGrouper;
 
   @Override
   @Transactional
@@ -35,20 +38,19 @@ public class JobExportInitiatedService implements Consumer<JobExportInitiated> {
     ExportFormat format = jobExportInitiated.getExportFormat();
     InternetAddress emailCC = jobExportInitiated.getEmailCC();
     var batches = annotationBatchService.findLatestPerTaskByJobId(jobId);
+    log.info("Found {} batches", batches.size());
     List<List<AnnotationBatch>> subBatches = split(batches);
-    var saved = createTasks(jobId, subBatches);
-
-    saved.forEach(
+    var savedTasks = createTasks(jobId, subBatches);
+    log.info("will create {} events", savedTasks.size());
+    savedTasks.forEach(
         task ->
             eventProducer.accept(
                 List.of(new ExportTaskCreated(jobId, task.getId(), format, emailCC))));
   }
 
   private List<List<AnnotationBatch>> split(List<AnnotationBatch> batches) {
-    if (batches.size() < MAX_HANDLED_BATCH_SIZE) {
-      return List.of(batches);
-    }
-    return Lists.partition(batches, BATCH_PARTITION_SIZE);
+    log.info("Splitting {} batches", batches.size());
+    return listGrouper.apply(batches, BATCH_PARTITION_SIZE);
   }
 
   private List<ExportTask> createTasks(String jobId, List<List<AnnotationBatch>> subBatches) {
