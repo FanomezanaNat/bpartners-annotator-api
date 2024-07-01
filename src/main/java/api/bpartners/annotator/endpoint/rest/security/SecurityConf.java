4c1aaa12fce2e969988e3ffd7e6d617008b02456
Onitsiky <hei.onitsiky@gmail.com>
2024-07-01 08:59:12 +0300

    chore: permitAll on poja endpoints

--- src/main/java/api/bpartners/annotator/endpoint/rest/security/SecurityConf.java
+++ src/main/java/api/bpartners/annotator/endpoint/rest/security/SecurityConf.java
@@ -81,7 +81,9 @@ public class SecurityConf {
                         new AntPathRequestMatcher("/health/bucket", GET.name()),
                         new AntPathRequestMatcher("/health/db", GET.name()),
                         new AntPathRequestMatcher("/health/email", GET.name()),
-                        new AntPathRequestMatcher("/health/event", GET.name())))),
+                        new AntPathRequestMatcher("/health/event1", GET.name()),
+                        new AntPathRequestMatcher("/health/event2", GET.name()),
+                        new AntPathRequestMatcher("/health/event/uuids", POST.name())))),
             AnonymousAuthenticationFilter.class)
         .authorizeHttpRequests(
             (authorize) ->
@@ -96,7 +98,11 @@ public class SecurityConf {
                     .permitAll()
                     .requestMatchers("/health/email")
                     .permitAll()
-                    .requestMatchers("/health/event")
+                    .requestMatchers("/health/event1")
+                    .permitAll()
+                    .requestMatchers("/health/event2")
+                    .permitAll()
+                    .requestMatchers(POST, "/health/event/uuids")
                     .permitAll()
                     .requestMatchers("/whoami")
                     .hasAnyRole(ADMIN.getRole(), ANNOTATOR.getRole())
